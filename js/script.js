af994b83fedcb86f0092b6b89fe7b43ad7d61854
EmanueleDiLeo <dileoemanuele11@gmail.com>
2023-09-18 18:07:48 +0200

    [ADD]: create boxs

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@ btn.addEventListener("click", function(){
   const difficulty = (document.querySelector("#difficulty").value).toLowerCase();
   const rowCol = rowColBox(difficulty);
   console.log(rowCol);
+  startBox(difficulty);
+  
+  
 
 });
 
@@ -29,4 +32,36 @@ function rowColBox(value){
     default:
       return "ERRORE"
   }
+}
+
+function startBox(difficulty){
+  let counter = 1;
+  const nRowCol = rowColBox(difficulty);
+  const widthBox = 100 / nRowCol;
+
+  for(let i = 1; i <= nRowCol; i++){
+    for(let i = 1; i <= nRowCol; i++){
+      const box = createBox(widthBox);
+      
+      // box.addEventListener("click",function(){
+      //   this.classList.toggle("click")
+      //   console.log(this.innerText);
+      // });
+      container.append(box);
+      counter++;
+    }
+  }
+
+  // const boxs = document.getElementsByClassName("square");
+
+
+
+}
+
+function createBox(widthBox){
+  const newBox = document.createElement('div');
+  newBox.className = "square";
+  newBox.style.width = widthBox + "%";
+  return newBox;
+  
 }
